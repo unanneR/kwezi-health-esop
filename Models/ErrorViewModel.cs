cb29e708d2e465e90ef2df9dd58e66005400e469
unanneR <ID+username@users.noreply.github.com>
2026-08-27 15:11:11 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kwezi_health_esop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
